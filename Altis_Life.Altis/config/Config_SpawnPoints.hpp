d7967ece315ec58db276c9f418825064f83991e1
OMGItsSnR <samspencer2002@outlook.com>
2017-01-28 21:30:57 +0000

    Update Config_SpawnPoints.hpp

--- Altis_Life.Altis/config/Config_SpawnPoints.hpp
+++ Altis_Life.Altis/config/Config_SpawnPoints.hpp
@@ -64,21 +64,21 @@ class CfgSpawnPoints {
 
         class Cop {
             class Kavala {
-                displayName = "Kavala HQ";
+                displayName = "Kavala PD";
                 spawnMarker = "cop_spawn_1";
                 icon = "\a3\ui_f\data\map\MapControl\watertower_ca.paa";
                 conditions = "";
             };
 
             class Athira {
-                displayName = "Athira HQ";
+                displayName = "Athira PD";
                 spawnMarker = "cop_spawn_3";
                 icon = "\a3\ui_f\data\map\MapControl\fuelstation_ca.paa";
                 conditions = "";
             };
 
             class Pyrgos {
-                displayName = "Pyrgos HQ";
+                displayName = "Pyrgos PD";
                 spawnMarker = "cop_spawn_2";
                 icon = "\a3\ui_f\data\map\GroupIcons\badge_rotate_0_gs.paa";
                 conditions = "";
